b2519d5089b66c59025cb676504b810d6c484dc2
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-10-08 10:15:22 +0700

    add APIs for discount

--- api-service/src/routes/index.js
+++ api-service/src/routes/index.js
@@ -4,9 +4,12 @@ const router = express.Router();
 const roleRouter = require('./role.routes');
 const userRouter = require('./user.routes');
 const contactRouter = require('./contact.routes');
+const discountRouter = require('./discount.routes');
 
 router.use('/roles', roleRouter);
 router.use('/users', userRouter);
 router.use('/contacts', contactRouter);
+router.use('/discounts', discountRouter);
+
 
 module.exports = router;
