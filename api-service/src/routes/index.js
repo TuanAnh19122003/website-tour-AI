b7cb9c26e21b8bd2d1c1867c00887ee510cdd47a
TuanAnh19122003 <gamethunghiem19122003@gmail.com>
2025-10-08 10:10:07 +0700

    add APIs for contact

--- api-service/src/routes/index.js
+++ api-service/src/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 
 const roleRouter = require('./role.routes');
 const userRouter = require('./user.routes');
+const contactRouter = require('./contact.routes');
 
 router.use('/roles', roleRouter);
 router.use('/users', userRouter);
+router.use('/contacts', contactRouter);
 
 module.exports = router;
